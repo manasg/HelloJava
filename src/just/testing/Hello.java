c86f0d352126773dd98b43aaa40f1d586b523b17
manasg <manas@easilydo.com>
2012-06-12 17:39:54 -0400

    Java.. oh java

---
+++ src/just/testing/Hello.java
@@ -0,0 +1,13 @@
+package just.testing;
+
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Lets pretend to do a lot & I like semi colons");
+
+	}
+
+}
