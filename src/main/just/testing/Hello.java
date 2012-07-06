8cdc9b07150418e3211e3271c31a4a400a6c1d42
Manas Gupta <manas.gupta@gmail.com>
2012-07-06 17:29:00 -0400

    Adding command line arg to main

--- src/main/just/testing/Hello.java
+++ src/main/just/testing/Hello.java
@@ -6,11 +6,16 @@ import java.util.Properties;
 public class Hello {
 
     public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
+            System.out.println("Need path to properties file as first arg!");
+            System.exit(1);
+        }
+        
         System.out.println("");
         System.out.println("Lets pretend to do a lot & I like semi colons");
 
         Properties props = new Properties();
-        FileInputStream in = new FileInputStream("some.properties");
+        FileInputStream in = new FileInputStream(args[0]);
         props.load(in);
 
         System.out.println(props.getProperty("msg"));
