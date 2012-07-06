695dc7b37685956c4fc5f146541c462c07db6acb
Manas Gupta <manas.gupta@gmail.com>
2012-07-06 16:34:57 -0400

    Spaces over tabs!

--- src/main/just/testing/Hello.java
+++ src/main/just/testing/Hello.java
@@ -5,16 +5,16 @@ import java.util.Properties;
 
 public class Hello {
 
-	public static void main(String[] args) throws Exception {
-		System.out.println("");
-		System.out.println("Lets pretend to do a lot & I like semi colons");
-        
+    public static void main(String[] args) throws Exception {
+        System.out.println("");
+        System.out.println("Lets pretend to do a lot & I like semi colons");
+
         Properties props = new Properties();
         FileInputStream in = new FileInputStream("some.properties");
         props.load(in);
 
-        System.out.println(props.getProperty("msg"));   
+        System.out.println(props.getProperty("msg"));
         System.out.println("");
-	}
+    }
 
 }
